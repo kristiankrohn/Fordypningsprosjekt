a1ca5bfa8d27ceb543ee47efe78a5528a279aec3
Kristian Krohn <kristianhk@msn.com>
2017-09-20 10:57:15 +0200

    plotting sux

--- OpenBCI/scripts/test_gui.py
+++ OpenBCI/scripts/test_gui.py
@@ -3,9 +3,15 @@ import open_bci_v3 as bci
 import os
 import logging
 import time
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib import style
+#import matplotlib.pyplot as plt
+#import matplotlib.animation as animation
+#from matplotlib import style
+import pyqtgraph
+from streamplot import PlotManager
+
+
+i = 0
+
 def printData(sample):
 	#os.system('cls')
 	#print "----------------"
@@ -22,38 +28,34 @@ def printData(sample):
 	#print sample.channel_data[8]
 	#print sample.aux_data
 	#print "----------------"
+	global i
+	global plt_mgr
+	print(i)
+	plt_mgr.add(name="Channel 1", x=i, y=sample.channel_data[0])
+	plt_mgr.add(name="Channel 2", x=i, y=sample.channel_data[1])
+	plt_mgr.add(name="Channel 3", x=i, y=sample.channel_data[2])
+	plt_mgr.add(name="Channel 4", x=i, y=sample.channel_data[3])
+	plt_mgr.add(name="Channel 5", x=i, y=sample.channel_data[4])
+	plt_mgr.add(name="Channel 6", x=i, y=sample.channel_data[5])
+	plt_mgr.add(name="Channel 7", x=i, y=sample.channel_data[6])
+	plt_mgr.add(name="Channel 8", x=i, y=sample.channel_data[7])
+	plt_mgr.update()
+	i = i + 1
 
-	xs.append(sample.id)
-	ys.append(sample.channel_data[0])
-	print(xs)
-	#ax1.clear()
-	#ax1.plot(xs, ys)
-	#ani = animation.FuncAnimation(fig, animate, interval=1000)
-	#plt.show()	
+#def main():
+port = 'COM6'
+baud = 115200
+logging.basicConfig(filename="test.log",format='%(asctime)s - %(levelname)s : %(message)s',level=logging.DEBUG)
+logging.info('---------LOG START-------------')
+board = bci.OpenBCIBoard(port=port, scaled_output=False, log=True)
+print("Board Instantiated")
+board.ser.write('v')
+time.sleep(10)
 
+plt_mgr = PlotManager(title="Hello plot")
 
-if __name__ == '__main__':
-	port = 'COM6'
-	#port = '/dev/tty.OpenBCI-DN008VTF'
-	#port = '/dev/tty.OpenBCI-DN0096XA'
-	baud = 115200
-	logging.basicConfig(filename="test.log",format='%(asctime)s - %(levelname)s : %(message)s',level=logging.DEBUG)
-	logging.info('---------LOG START-------------')
-	board = bci.OpenBCIBoard(port=port, scaled_output=False, log=True)
-	print("Board Instantiated")
+board.start_streaming(printData)
+plt_mgr.close()
 
-	board.ser.write('v')
-	time.sleep(10)
-
-	
-	style.use('fivethirtyeight')
-	xs = []
-	ys = []
-	fig = plt.figure()
-	ax1 = fig.add_subplot(1,1,1)
-
-	board.start_streaming(printData)
-	#board.print_bytes_in()
-
-
-
+#if __name__ == '__main__':
+	#main()
