cb498c0871d0210fc7b3c8a66f0b949c2e0fabab
Kristian Krohn <kristianhk@msn.com>
2017-09-21 13:23:02 +0200

    noobing

--- OpenBCI/scripts/test_gui.py
+++ OpenBCI/scripts/test_gui.py
@@ -2,61 +2,29 @@ import sys; sys.path.append('..') # help python find open_bci_v3.py relative to 
 import open_bci_v3 as bci
 import os
 import logging
-import time
-#import matplotlib.pyplot as plt
-#import matplotlib.animation as animation
-#from matplotlib import style
+import time as tme
 from pyqtgraph.Qt import QtGui, QtCore
-from streamplot import PlotManager
+import numpy as np
+import pyqtgraph as pg
+from pyqtgraph.ptime import time
 
 
-i = 0
+app = QtGui.QApplication([])
+p = pg.plot()
+nPlots = 8
+nSamples = 500
+p.setWindowTitle('pyqtgraph example: MultiPlotSpeedTest')
+#p.setRange(QtCore.QRectF(0, -10, 5000, 20)) 
+p.setLabel('bottom', 'Index', units='B')
+#curves = [p.plot(pen=(i,nPlots*1.3)) for i in range(nPlots)]
+curves = []
+ptr = 0
+lastTime = time()
+fps = None
+count = 0
+data = np.array([])
 
-def printData(sample):
-	#os.system('cls')
-	#print "----------------"
-	#print("%f" %(sample.id))
-	#print sample.channel_data
-	#print sample.channel_data[0]
-	#print sample.channel_data[1]
-	#print sample.channel_data[2]
-	#print sample.channel_data[3]
-	#print sample.channel_data[4]
-	#print sample.channel_data[5]
-	#print sample.channel_data[6]
-	#print sample.channel_data[7]
-	#print sample.channel_data[8]
-	#print sample.aux_data
-	#print "----------------"
-	global i
-	global plt_mgr
-	#print(i)
-	plt_mgr.add(name="Channel 1", x=i, y=sample.channel_data[0])
-	plt_mgr.add(name="Channel 2", x=i, y=sample.channel_data[1])
-	plt_mgr.add(name="Channel 3", x=i, y=sample.channel_data[2])
-	plt_mgr.add(name="Channel 4", x=i, y=sample.channel_data[3])
-	plt_mgr.add(name="Channel 5", x=i, y=sample.channel_data[4])
-	plt_mgr.add(name="Channel 6", x=i, y=sample.channel_data[5])
-	plt_mgr.add(name="Channel 7", x=i, y=sample.channel_data[6])
-	plt_mgr.add(name="Channel 8", x=i, y=sample.channel_data[7])
-	plt_mgr.update()
-	i = i + 1
 
-#p6 = win.addPlot(title="Updating plot")
-#curve = p6.plot(pen='y')
-#data = np.random.normal(size=(10,1000))
-#ptr = 0
-#def update():
-    #global curve, data, ptr, p6
-    #curve.setData(data[ptr%10])
-    #if ptr == 0:
-        #p6.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
-    #ptr += 1
-#timer = QtCore.QTimer()
-#timer.timeout.connect(update)
-#timer.start(50)
-
-#def main():
 port = 'COM6'
 baud = 115200
 logging.basicConfig(filename="test.log",format='%(asctime)s - %(levelname)s : %(message)s',level=logging.DEBUG)
@@ -64,12 +32,83 @@ logging.info('---------LOG START-------------')
 board = bci.OpenBCIBoard(port=port, scaled_output=False, log=True)
 print("Board Instantiated")
 board.ser.write('v')
-time.sleep(10)
+tme.sleep(10)
+if not board.streaming:
+	board.ser.write(b'b')
+	board.streaming = True
 
-plt_mgr = PlotManager(title="Hello plot")
+	
 
-board.start_streaming(printData)
-plt_mgr.close()
 
-#if __name__ == '__main__':
-	#main()
+def update():
+	global curve, data, ptr, p, lastTime, fps, nPlots, count, board
+	count += 1
+
+	sample = board._read_serial_binary()
+	data = np.append(data, sample.channel_data)
+	if data.shape[0] >= nSamples:
+		data = np.delete(data, data[0:7:1])
+		print("deliting")
+	print(data)
+	print(data.shape[0])
+    #print "---------", count
+	for i in range(nPlots):
+		#curves[i].setData(data[(ptr+i)%data.shape[0]])
+		curves[i].setData(data[(ptr+i)%data.shape[0]])
+    #print "   setData done."
+	ptr += nPlots
+	now = time()
+	dt = now - lastTime	
+	lastTime = now
+	if fps is None:
+		fps = 1.0/dt
+	else:
+		s = np.clip(dt*3., 0, 1)
+		fps = fps * (1-s) + (1.0/dt) * s
+	p.setTitle('%0.2f fps' % fps)
+    #app.processEvents()  ## force complete redraw for every plot
+
+
+def main():
+
+	#mw = QtGui.QMainWindow()
+	#mw.resize(800,800)
+
+	
+
+
+
+	for i in range(nPlots):
+		c = pg.PlotCurveItem(pen=(i,nPlots*1.3))
+		p.addItem(c)
+		c.setPos(0,i*6)
+		curves.append(c)
+
+	p.setYRange(0, nPlots*6)
+	p.setXRange(0, nSamples)
+	p.resize(600,900)
+
+	rgn = pg.LinearRegionItem([nSamples/5.,nSamples/3.])
+	p.addItem(rgn)
+
+	sample = board._read_serial_binary()
+	data = sample.channel_data
+
+
+
+
+	timer = QtCore.QTimer()
+	timer.timeout.connect(update)
+	timer.start(0)
+
+	#while True:
+		#sample = board._read_serial_binary()
+		#data = sample.channel_data
+		#print(sample.channel_data)		
+	QtGui.QApplication.instance().exec_()
+		
+if __name__ == '__main__':
+	main()
+	#import sys
+	#if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+		#QtGui.QApplication.instance().exec_()
