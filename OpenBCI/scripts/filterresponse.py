13c854c9a8ff71dc99b35d07c31732f4c095f5dc
Kristian Krohn <kristianhk@msn.com>
2017-10-24 16:29:38 +0200

    plot all function and minor cleanup

--- OpenBCI/scripts/filterresponse.py
+++ OpenBCI/scripts/filterresponse.py
@@ -1,20 +1,31 @@
 from scipy import signal
+from scipy.signal import kaiserord, firwin
 import numpy as np
 import matplotlib.pyplot as plt
 
-window = 75
+#window = 75
 fs = 250.0
-f0 = 50.0
-Q = 50
-w0 = f0/(fs/2)
-b, a = signal.iirnotch(w0, Q)
+#f0 = 50.0
+#Q = 50
+#w0 = f0/(fs/2)
+#b, a = signal.iirnotch(w0, Q)
 
+nyq_rate = fs / 2.0
+N = 99
+ripple_db = signal.kaiser_atten(N, 1)
+beta = signal.kaiser_beta(ripple_db)
 
+# The cutoff frequency of the filter.
+cutoff_hz = 3.0
+
+# Use firwin with a Kaiser window to create a lowpass FIR filter.
+a = firwin(N, cutoff_hz/nyq_rate, pass_zero=False, window = 'hann')
+#a = -a
+#a[n/2] = a[n/2] + 1
 #N  = 30    # Filter order
 #fk = 30
 #Wn = fk/(fs/2) # Cutoff frequency
 #b, a = signal.butter(N, Wn, output='ba')
-
 
 #order = 2
 #hc = 40.0/(fs/2)
@@ -25,7 +36,7 @@ b, a = signal.iirnotch(w0, Q)
 
 
 
-w, h = signal.freqz(b, a)
+w, h = signal.freqz(a)
  # Generate frequency axis
 freq = w*fs/(2*np.pi)
  # Plot
@@ -34,13 +45,13 @@ ax[0].plot(freq, 20*np.log10(abs(h)), color='blue')
 ax[0].set_title("Notch filter")
 ax[0].set_ylabel("Amplitude (dB)", color='blue')
 ax[0].set_xlim([0, 100])
-ax[0].set_ylim([-25, 10])
+ax[0].set_ylim([-100, 10])
 ax[0].grid()
 ax[1].plot(freq, np.unwrap(np.angle(h))*180/np.pi, color='green')
 ax[1].set_ylabel("Angle (degrees)", color='green')
 ax[1].set_xlabel("Frequency (Hz)")
 ax[1].set_xlim([0, 100])
 ax[1].set_yticks([-90, -60, -30, 0, 30, 60, 90])
-ax[1].set_ylim([-90, 90])
+ax[1].set_ylim([-10080, 90])
 ax[1].grid()
 plt.show()
--- OpenBCI/scripts/test_gui.py
+++ OpenBCI/scripts/test_gui.py
@@ -11,8 +11,21 @@ import threading
 from threading import Lock
 from scipy import signal
 import matplotlib.pyplot as plt
+from numpy.random import randn
 
-
+z = randn(2000)
+#legends = []
+#for i in range(2):
+	#label = "Fp %d" %(i+1)
+	#print(label)
+	#label = tuple([label])
+	#legend, = plt.plot(z, label=label)
+	#legends.append(legend)
+#plt.ylabel('uV')
+#plt.xlabel('Sample')
+#plt.legend(handles=legends)
+#legends = []
+#plt.show()
 
 mutex = Lock()
 
@@ -71,7 +84,7 @@ print("Graphsetup finished")
 
 #Filtersetup
 #Notchfilter
-window = 75
+window = 150
 fs = 250.0
 f0 = 50.0
 Q = 50
@@ -86,8 +99,8 @@ bandpassFilter = False
 
 
 sample = board._read_serial_binary()
-notchZi = np.array([[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]])
-
+#notchZi = np.array([[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]])
+notchZi = np.zeros([8,2])
 init = True
 
 #Butterworth lowpass filter
@@ -95,18 +108,22 @@ N  = 5    # Filter order
 fk = 30
 Wn = fk/(fs/2) # Cutoff frequency
 lowpassB, lowpassA = signal.butter(N, Wn, output='ba')
-lowpassZi = np.array([[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]])
-
+#lowpassZi = np.array([[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]])
+lowpassZi = np.zeros([8,N])
 #Butterworth bandpass filter
 order = 2
 hc = 100.0/(fs/2) #High cut
 lc = 1.0/(fs/2)	#Low cut
-bandpassB, bandpassA = signal.butter(order, [lc, hc], btype='band', output='ba', analog=False)
-bandpassZi = np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
+#bandpassB, bandpassA = signal.butter(order, [lc, hc], btype='band', output='ba', analog=False)
+#bandpassZi = np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
 
-print(bandpassB)
-print(bandpassA)
-
+#FIR Highpass
+cutoff_hz = 1.0
+nyq_rate = fs/2
+# Use firwin with a Kaiser window to create a lowpass FIR filter.
+bandpassB = signal.firwin(window-1, cutoff_hz/nyq_rate, pass_zero=False, window = 'hann')
+bandpassA = 1.0 #np.ones(len(bandpassA))
+bandpassZi = np.zeros([8, window-2])
 print("Filtersetup finished")
 
 
@@ -133,7 +150,7 @@ def printData(sample):
 				averagedata[i].append(sample.channel_data[i]-average)
 				#averagedata[i].append(sample.channel_data[i])
 			else:
-				data[i].append(sample.channel_data[i]-average)
+				data[i].append(sample.channel_data[i])
 				
 		if len(data[0]) >= 2000:
 			for i in range(nPlots):
@@ -156,32 +173,37 @@ def notchFilter():
 	global lowpassB, lowpassA, lowpassZi 
 	global bandpassB, bandpassA, bandpassZi
 	global notchB, notchA, notchZi 
-	global averagedata, data, window, init
+	global averagedata, data, window, init, initNotch, initLowpass, initBandpass
 	global bandstopFilter, lowpassFilter, bandpassFilter
 	
 	with(mutex):
-
-		for i in range(nPlots):
-			if init == True: #Gjor dette til en funksjon, input koeff, return zi
+		
+		if init == True: #Gjor dette til en funksjon, input koeff, return zi
+			for i in range(nPlots):
 				notchZi[i] = signal.lfilter_zi(notchB, notchA) * averagedata[i][0]
 				lowpassZi[i] = signal.lfilter_zi(lowpassB, lowpassA) * averagedata[i][0]
 				bandpassZi[i] = signal.lfilter_zi(bandpassB, bandpassA) * averagedata[i][0]
-				init = False
+			init = False
 
+			#TODO: init filters again when turned on
 		for i in range(nPlots):
 			x = averagedata[i]
 
 			if bandstopFilter:
-				#x, notchZi[i] = signal.lfilter(notchB, notchA, x, zi=notchZi[i])
-				x = signal.filtfilt(notchB, notchA, x, irlen = 74)
+				x, notchZi[i] = signal.lfilter(notchB, notchA, x, zi=notchZi[i])
+				#x = signal.filtfilt(notchB, notchA, x, irlen = 74)
 
 			if lowpassFilter:
-				#x, lowpassZi[i] = signal.lfilter(lowpassB, lowpassA, x, zi=lowpassZi[i])
-				x = signal.filtfilt(lowpassB, lowpassA, x, irlen = 74)
+				x, lowpassZi[i] = signal.lfilter(lowpassB, lowpassA, x, zi=lowpassZi[i])
+				#x = signal.filtfilt(lowpassB, lowpassA, x, irlen = 74)
 				
 			if bandpassFilter:
-				#x, bandpassZi[i] = signal.lfilter(bandpassB, bandpassA, x, zi=bandpassZi[i])
-				x = signal.filtfilt(bandpassB, bandpassA, x, irlen = 74)
+				x, bandpassZi[i] = signal.lfilter(bandpassB, bandpassA, x, zi=bandpassZi[i])
+				
+				#print(len(returnZi))
+
+				#x = signal.lfilter(bandpassB, 1.0, x)
+				#x = signal.filtfilt(bandpassB, bandpassA, x, irlen = 74)
 
 			appendData(x,i)
 
@@ -189,9 +211,31 @@ def notchFilter():
 		averagedata = [],[],[],[],[],[],[],[]
 
 def plot():
+	legends = []
 	for i in range(2):
-		plt.plot(data[i])
+		label = "Fp %d" %(i+1)
+		#print(label)
+		#label = tuple([label])
+		legend, = plt.plot(data[i], label=label)
+		legends.append(legend)
 	plt.ylabel('uV')
+	plt.xlabel('Sample')
+	plt.legend(handles=legends)
+	legends = []
+	plt.show()
+
+def plotAll():
+	legends = []
+	for i in range(nPlots):
+		label = "Channel %d" %(i+1)
+		#print(label)
+		#label = tuple([label])
+		legend, = plt.plot(data[i], label=label)
+		legends.append(legend)
+	plt.ylabel('uV')
+	plt.xlabel('Sample')
+	plt.legend(handles=legends)
+	legends = []
 	plt.show()
 
 def update():
@@ -244,7 +288,8 @@ def keys():
 			sys.exit()
 		elif string == "plot":
 			plot()
-
+		elif string == "plotall":
+			plotAll()
 
 def main():
 
