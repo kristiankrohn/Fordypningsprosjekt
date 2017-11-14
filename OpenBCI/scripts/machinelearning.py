cc6278998275f338d345c7f44edebb8897b44d35
Adrian <adrian@ribe.no>
2017-11-14 18:17:39 +0100

    Added feature-calculation from the dataset. Just a few features such as Variance, std, mean, max, min etc. Remember to check temp.txt if prediction doesn't work. Also has a temporary visualization part in extractFeatures(), which are supposed to help us see some consistency of the movements

--- OpenBCI/scripts/machinelearning.py
+++ OpenBCI/scripts/machinelearning.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
 from sklearn import svm
-from sklearn.cluster import KMeans
 from sklearn.externals import joblib #in order to store the svm
 
 DataSet = []
@@ -18,9 +17,10 @@ convertedPredictData = [[]]
 def main():
     AllData, predictData = readFile()
     arrangeDataset(AllData, predictData)
-    clf = createAndTrain()
+    extractFeatures() #calculate different features of time-series.
+    clf = createAndTrain() #switch to loadAndTrain() if you want to load previous machine-learning-state
     saveMachinestate(clf) #if you want to save the state of classifier
-    predict(clf)
+    predict(clf) #predict with given classifier.
 
 
 def readFile():
@@ -46,11 +46,59 @@ def appendLabel(featuretype):
         DataSetLabels.append(4)
 
 
+
+def extractFeatures():
+    global convertedDataSet, DataSetLabels
+    standardSet = np.std(convertedDataSet, axis = 1) #prints the standard-deviation of the 500 samples, for all movementdirections
+    covSet = np.cov(convertedDataSet, rowvar = True)
+    mean = np.mean(convertedDataSet, axis = 1)
+    var = np.var(convertedDataSet, axis = 1)
+    #minimumCh1Ch2 = np.fmin(convertedDataSetChannel1, convertedDataSetChannel2)#can be checked to make an array of the minimum value between sample n in both channels
+    minimum = np.amin(convertedDataSet, axis = 1)#find minimum of all 500 samples for the 23 directions.
+    maximum = np.amax(convertedDataSet, axis = 1)#find maximum of all 500 samples for the 23 directions
+    #absolute = np.absolute(convertedDataSet) Blir et 2D-array fordi den tar absoluttverdien til alle samples, for alle retninger. What to do??
+    #print(var)
+    #print(len(var))
+
+
+    #from here and down is for visualization purposes only.
+    Y = np.array(map(float, DataSetLabels))
+    visuRight = []
+    visuLeft = []
+    visuUp = []
+    visuDown = []
+    visuCenter= []
+
+    for i in range(len(var)):
+        if Y[i] == 0:
+            visuRight.append(mean[i])
+        if Y[i] == 1:
+            visuUp.append(mean[i])
+        if Y[i] == 2:
+            visuLeft.append(mean[i])
+        if Y[i] == 3:
+            visuDown.append(mean[i])
+        if Y[i] == 4:
+            visuCenter.append(mean[i])
+
+
+    print("Right:")
+    print(visuRight)
+    print("Left:")
+    print(visuLeft)
+    print("Up:")
+    print(visuUp)
+    print("Down:")
+    print(visuDown)
+    print("Center:")
+    print(visuCenter)
+
 def arrangeDataset(AllData, predictData):
     global Dataset, predictSet, convertedDataSet, convertedPredictData
 
     DataSet = AllData.split(':')
     predictSet = predictData.split(':')
+
 
     for i in range(len(DataSet)):
         feature = []
@@ -73,19 +121,19 @@ def arrangeDataset(AllData, predictData):
 
             convertedPredictData.append(map(float, predictionFeature))
 
+
             #pops the two empty lists at index 0 and end-index
             if i == (len(predictSet) - 1):
                 convertedPredictData.pop(0)
                 convertedPredictData.pop(len(predictSet) - 1)
-
 
 def createAndTrain():
     global convertedDataSet, convertedPredictData, DataSetLabels
 
     X = np.array(convertedDataSet)
     Y = np.array(map(float, DataSetLabels))
-    print(Y)
-
+    #print(Y)
+    #print(len(Y))
     clf = LinearSVC()
     clf.fit(X,Y)
     return clf
@@ -110,7 +158,8 @@ def predict(clf):
     global convertedPredictData
 
     Z = np.array(convertedPredictData)
-    print(clf.predict(Z))
+    print(len(Z))
+    print(clf.predict(Z))#if this doesn't work, check readFile().
 
 
 
