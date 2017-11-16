c349eb76aa034bf8c3647be092b37cb293ea847f
Adrian <adrian@ribe.no>
2017-11-16 10:44:29 +0100

    har nå lagt til grafing av featuresene til dataen vi får inn. Dette gjøres i funksjonen visualizeFeature()

--- OpenBCI/scripts/machinelearning.py
+++ OpenBCI/scripts/machinelearning.py
@@ -21,108 +21,22 @@ def main():
     AllData, predictData = readFile()
     arrangeDataset(AllData, predictData)
     extractFeatures() #calculate different features of time-series.
-    #clf = createAndTrain() #switch to loadAndTrain() if you want to load previous machine-learning-state
+    clf = createAndTrain() #switch to loadAndTrain() if you want to load previous machine-learning-state
     #saveMachinestate(clf) #if you want to save the state of classifier
-    #predict(clf) #predict with given classifier.
+    predict(clf) #predict with given classifier.
 
 
 def readFile():
 
     file = open('data.txt', 'r')
-    pFile = open('temp.txt', 'r')
+    pFile = open('data.txt', 'r')
     AllData = file.read()
     predictData = pFile.read()
     return AllData, predictData
 
-def appendLabel(featuretype):
-    global DataSetLabels
-
-    if featuretype == 'r1':
-        DataSetLabels.append(0)
-    if featuretype == 'u1':
-        DataSetLabels.append(1)
-    if featuretype == 'l1':
-        DataSetLabels.append(2)
-    if featuretype == 'd1':
-        DataSetLabels.append(3)
-    if featuretype == 'c1':
-        DataSetLabels.append(4)
 
 
 
-def extractFeatures():
-    global convertedDataSet
-    standardSet = np.std(convertedDataSet, axis = 1) #prints the standard-deviation of the 500 samples, for all movementdirections
-    covSet = np.cov(convertedDataSet, rowvar = True)
-    mean = np.mean(convertedDataSet, axis = 1)
-    var = np.var(convertedDataSet, axis = 1)
-    #minimumCh1Ch2 = np.fmin(convertedDataSetChannel1, convertedDataSetChannel2)#can be checked to make an array of the minimum value between sample n in both channels
-    minimum = np.amin(convertedDataSet, axis = 1)#find minimum of all 500 samples for the 23 directions.
-    maximum = np.amax(convertedDataSet, axis = 1)#find maximum of all 500 samples for the 23 directions
-    #absolute = np.absolute(convertedDataSet) Blir et 2D-array fordi den tar absoluttverdien til alle samples, for alle retninger. What to do??
-
-
-    #from here and down is for visualization purposes only.
-    visualizeFeature(var, "var")
-
-    #print("Right:")
-    #print(visuRight)
-    #print("Left:")
-    #print(visuLeft)
-    #print("Up:")
-    #print(visuUp)
-    #print("Down:")
-    #print(visuDown)
-    #print("Center:")
-    #print(visuCenter)
-
-
-def visualizeFeature(visu, string):
-    global DataSetLabels
-
-
-    visuValue = []
-    visuDirection = []
-    visuRight = []
-    visuLeft = []
-    visuUp = []
-    visuDown = []
-    visuCenter = []
-
-
-    Y = np.array(map(float, DataSetLabels))
-
-    for i in range(len(Y)):
-        visuValue.append(visu[i])
-        if Y[i] == 0:
-            visuDirection.append('Right')
-        if Y[i] == 1:
-            visuDirection.append('Up')
-        if Y[i] == 2:
-            visuDirection.append('Left')
-        if Y[i] == 3:
-            visuDirection.append('Down')
-        if Y[i] == 4:
-            visuDirection.append('Center')
-
-
-    dictionary = { '%s' % string: visuValue, 'Direction': visuDirection}
-    dataframe = pd.DataFrame(columns = ['Direction' , '%s' % string])
-
-
-    dataframe['Direction'] = visuDirection
-    dataframe['%s' % string] = visuValue
-    print(dataframe)
-    #print(dataframe.columns.get_loc("var"))
-    #dataframe = pd.DataFrame.from_dict(dictionary, orient = 'index')
-
-    #print(string + ":")
-    #print(dataframe)
-    print('Direction' in dataframe.index)
-
-    #print(dataframe['Direction'])
-    sns.barplot(data=dataframe, y='Direction')
-    #plt.show()
 
 def arrangeDataset(AllData, predictData):
     global Dataset, predictSet, convertedDataSet, convertedPredictData
@@ -139,6 +53,7 @@ def arrangeDataset(AllData, predictData):
         feature.pop(0)
         convertedDataSet.append(map(float, feature))
         appendLabel(featuretype)
+
 
         #pops the two empty lists at index 0 and end-index
         if i == (len(DataSet) - 1):
@@ -158,13 +73,28 @@ def arrangeDataset(AllData, predictData):
                 convertedPredictData.pop(0)
                 convertedPredictData.pop(len(predictSet) - 1)
 
+def appendLabel(featuretype):
+    global DataSetLabels
+
+    if featuretype == 'r1' or featuretype == 'r0':
+        DataSetLabels.append(0)
+    if featuretype == 'u1' or featuretype == 'u0':
+        DataSetLabels.append(1)
+    if featuretype == 'l1' or featuretype == 'l0':
+        DataSetLabels.append(2)
+    if featuretype == 'd1' or featuretype == 'd0':
+        DataSetLabels.append(3)
+    if featuretype == 'c1' or featuretype == 'c0':
+        DataSetLabels.append(4)
+
+
+
 def createAndTrain():
     global convertedDataSet, convertedPredictData, DataSetLabels
 
     X = np.array(convertedDataSet)
     Y = np.array(map(float, DataSetLabels))
     #print(Y)
-    #print(len(Y))
     clf = LinearSVC()
     clf.fit(X,Y)
     return clf
@@ -191,6 +121,57 @@ def predict(clf):
     Z = np.array(convertedPredictData)
     print(len(Z))
     print(clf.predict(Z))#if this doesn't work, check readFile().
+
+
+def extractFeatures():
+    global convertedDataSet
+    standardSet = np.std(convertedDataSet, axis = 1) #prints the standard-deviation of the 500 samples, for all movementdirections
+    covSet = np.cov(convertedDataSet, rowvar = True)
+    mean = np.mean(convertedDataSet, axis = 1)
+    var = np.var(convertedDataSet, axis = 1)
+    #minimumCh1Ch2 = np.fmin(convertedDataSetChannel1, convertedDataSetChannel2)#can be checked to make an array of the minimum value between sample n in both channels
+    minimum = np.amin(convertedDataSet, axis = 1)#find minimum of all 500 samples for the 23 directions.
+    maximum = np.amax(convertedDataSet, axis = 1)#find maximum of all 500 samples for the 23 directions
+    #absolute = np.absolute(convertedDataSet) Blir et 2D-array fordi den tar absoluttverdien til alle samples, for alle retninger. What to do??
+
+
+    #from here and down is for visualization purposes only.
+    visualizeFeature(var, "var")
+
+def visualizeFeature(visu, string):
+    global DataSetLabels
+
+    visuValue = []
+    visuDirection = []
+
+    Y = np.array(map(float, DataSetLabels))
+
+    for i in range(len(Y)):
+        visuValue.append(visu[i])
+        if Y[i] == 0:
+            visuDirection.append('Right')
+
+        if Y[i] == 1:
+            visuDirection.append('Up')
+
+        if Y[i] == 2:
+            visuDirection.append('Left')
+
+        if Y[i] == 3:
+            visuDirection.append('Down')
+
+        if Y[i] == 4:
+            visuDirection.append('Center')
+
+
+    dictionary = {'Direction': visuDirection, '%s' % string: visuValue}
+    dataframe = pd.DataFrame(dictionary, columns = ['Direction', '%s' % string])
+    #print(dataframe)
+    g = sns.violinplot(y = dataframe.Direction, x = '%s' % string, data=dataframe, inner='quartile')
+    savestring = "featureFigures/" + string + ".png"
+    plt.savefig(savestring, bbox_inches='tight')
+    plt.close()
+
 
 
 
